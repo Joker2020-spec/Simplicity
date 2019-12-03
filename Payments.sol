12d6836e3f0aa5b74d96f4dfc5d96326e5357b49
Joker2020-spec <57395694+Joker2020-spec@users.noreply.github.com>
2019-12-03 14:24:49 +1000

    Update Payments.sol

--- Payments.sol
+++ Payments.sol
@@ -17,16 +17,28 @@ contract Payments is TenantFactory {
         uint payment_number;
         bool payed;
         address payer;
+        address payee;
+        bytes32 tx_hash;
     }
     
     mapping (address => mapping(uint => Payment)) payments_made;
     mapping (uint => mapping(uint => address)) payed_too;
     
-    function makePayment(uint _amount, uint _time, uint _finish_date, address _payee) public returns (bool success) {
+    function makePayment(uint _amount, uint _time, uint _finish_date, address _payee, address _too, bytes32 tx_id) public returns (bool success) {
         require (_finish_date > now && _finish_date > MIN_PAYMENT_TERMS);
         require (MAX_PAYMENT_TERMS >= _finish_date); 
         TOTAL_PAYMENTS_MADE + 1;
-        Payment memory payment = Payment({time: _time, payable_amount: _amount, start_date: now, finish_date: _finish_date, payment_number: TOTAL_PAYMENTS_MADE, payed: true, payer: msg.sender});
+        Payment memory payment = Payment({
+            time: _time, 
+            payable_amount: _amount, 
+            start_date: now, 
+            finish_date: _finish_date, 
+            payment_number: TOTAL_PAYMENTS_MADE, 
+            payed: true, 
+            payer: msg.sender,
+            payee: _too,
+            tx_hash: tx_id
+        });
         payments_made[msg.sender][_amount] = payment;
         payed_too[TOTAL_PAYMENTS_MADE][_amount] = _payee;
         return success;
